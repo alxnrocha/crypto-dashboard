a3fe41d1d538fdb0019c3845713a83e38ee43f72
Alexandre Rocha <alxnrocha@gmail.com>
2026-07-08 15:00:24 +0200

    feat(ui): implement watchlist page (closes #13)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { DashboardLayout } from './components/layout/DashboardLayout';
 import { Home } from './pages/Home';
+import { Watchlist } from './pages/Watchlist';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<DashboardLayout />}>
           <Route index element={<Home />} />
+          <Route path="watchlist" element={<Watchlist />} />
         </Route>
       </Routes>
     </BrowserRouter>
