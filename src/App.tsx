6eabd6ea564533dd1d9a9e73b5f6361ae629f8bd
Alexandre Rocha <alxnrocha@gmail.com>
2026-08-22 19:29:52 +0200

    feat(ui): integrate native verified project badge with 5s dismiss interval into core application layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { ProjectBadge } from './components/ProjectBadge';
 import { HashRouter, Route, Routes } from "react-router-dom";
 import { DashboardLayout } from "./components/layout/DashboardLayout";
 import { Home } from "./pages/Home";
